0a6237495a75eb0018f30d26d1e4b27ff28dd5d5
yuqian <584882416@qq.com>
2022-09-28 14:49:02 +0800

    假如parcel ,将Button组件改为单文件组件

---
+++ src/Button.vue
@@ -0,0 +1,28 @@
+<template>
+  <button class="g-button">按钮</button>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="scss">
+.g-button{
+  height: 32px;
+  padding: 0 1em;
+  font:inherit;
+  border-radius: 4px;
+  border:1px solid #999;
+  background: white;
+
+&:hover{
+  border-color:#666;
+}
+&:active{
+  background-color:#999;
+}
+&:focus{
+outline:none
+}
+}
+</style>
---
+++ src/app.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import Button from './Button'
+Vue.component('g-button',Button)
+new Vue({
+    el:"#app",
+})
