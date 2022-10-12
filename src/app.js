4ee13adfb2779c4385d6bf89be934a348437dbb1
yuqian <584882416@qq.com>
2022-10-12 11:49:17 +0800

    重构row and col

--- src/app.js
+++ src/app.js
@@ -3,21 +3,26 @@ import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-Group'
 import Input from './input'
+import Row from './row'
+import Col from './column'
 Vue.component('g-button',Button)
 Vue.component('g-icon',Icon)
 Vue.component('g-button-group',ButtonGroup)
 Vue.component('g-input',Input)
+Vue.component('g-row',Row)
+Vue.component('g-col',Col)
 new Vue({
     el:"#app",
-   data:{
+  data:{
       loading1:false,
        loading2:false,
-       message:'hi'
+       message:'hi',
     },
     methods:{
         inputChange(e,x){
             console.log(e.target.value);
             console.log(x);
-        }
+        },
+
     }
 })
---
+++ src/row.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="row" :style="rowStyle">
+  <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  props:{
+    gutter:{
+      type:[Number,String]
+    }
+  },
+  computed:{
+    rolStyle(){
+      return {marginLeft: -this.gutter/2+'px', marginRight: -this.gutter/2+'px'}
+    }
+  },
+  mounted(){
+    this.$children.forEach((vm) => {
+      vm.gutter = this.gutter
+    })
+  }
+
+}
+</script>
+<style lang="scss" scoped>
+.row{
+  display: flex;
+}
+</style>
