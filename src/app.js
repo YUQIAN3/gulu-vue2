f720209a3734e0de39775c270ee90c582b17fb42
yuqian <584882416@qq.com>
2022-10-24 14:14:24 +0800

    创建了popover组件

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ import TabsHead from './tabs-head'
 import TabsBody from './tabs-body'
 import TabsItem from './tabs-item'
 import TabsPane from './tabs-pane'
+import Popover from './popover'
 
 Vue.component('g-button',Button)
 Vue.component('g-icon',Icon)
@@ -25,6 +26,7 @@ Vue.component('g-tabs-head',TabsHead)
 Vue.component('g-tabs-body',TabsBody)
 Vue.component('g-tabs-item',TabsItem)
 Vue.component('g-tabs-pane',TabsPane)
+Vue.component('g-popover',Popover)
 Vue.use(plugin)
 new Vue({
     el:"#app",
