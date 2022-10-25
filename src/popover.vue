b3c37f0238a93f14dc5cdbed7a5831fe86a0f4c2
yuqian <584882416@qq.com>
2022-10-25 10:27:48 +0800

    实现了最简单的popover

--- src/popover.vue
+++ src/popover.vue
@@ -1,10 +1,11 @@
 <template>
 <div class="popover" @click="xxx">
-  <div class="content-wrapper" v-if="visible">
+  <div ref="contentWrapper"  class="content-wrapper" v-if="visible">
     <slot name="content" ></slot>
   </div>
-
+<span ref="triggerWrapper">
   <slot></slot>
+</span>
 </div>
 </template>
 <script>
@@ -14,12 +15,32 @@ export default {
       visible:false
     }
   },
+  mounted(){
+    console.log(this.$refs.triggerWrapper);
+  },
   methods:{
     xxx(){
       this.visible=!this.visible
-      console.log(this.visible);
+      console.log('切换visible');
+      if(this.visible===true) {
+          this.$nextTick(()=>{
+           document.body.appendChild(this.$refs.contentWrapper)
+            let{width,height,top,left}=this.$refs.triggerWrapper.getBoundingClientRect()
+           this.$refs.contentWrapper.style.left=left+'px'
+            this.$refs.contentWrapper.style.top=`${top}px`
+
+          let x=()=>{
+            this.visible = false
+            document.removeEventListener('click',x)
+            console.log('删除 document监听器');
+            console.log('点击body就关闭popover');
+          }
+          document.addEventListener('click',x)
+
+      })
+      }
+      }
     }
-  }
 }
 </script>
 <style lang="scss" scoped>
@@ -27,11 +48,12 @@ export default {
   display: inline-block;
   vertical-align: top;
   position: relative;
-  >.content-wrapper{
-    position:absolute;
-    bottom:100%;
-    border:1px solid red;
-    box-shadow: 0 0 3px rgba(0,0,0,0.5);
-  }
 }
+  .content-wrapper{
+    position:absolute;
+    border:3px solid red;
+    box-shadow: 0 0 3px rgba(0,0,0,0.5);
+    transform: translateY(-100%);
+  }
+
 </style>
