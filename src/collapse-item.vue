c56410e0efe0af6ebdd58f1edbcbbd9b8ed6cec7
yuqian <584882416@qq.com>
2022-10-27 15:40:33 +0800

    collapse基本样式和基本功能完成

---
+++ src/collapse-item.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="collapseItem" >
+    <div class="title" @click="open=!open" >
+      {{title}}
+    </div>
+    <div class="content" v-if="open">
+      <slot></slot>
+    </div>
+
+  </div>
+</template>
+<script>
+export default {
+props:{
+  title:{
+    type:String,
+    required:true,
+  }
+},
+  data(){
+  return{open:false}
+  }
+}
+</script>
+<style lang="scss" scoped>
+.collapseItem{
+ > .title{
+   border:1px solid #ddd;
+   margin-top: -1px;
+   margin-left: -1px;
+   margin-right: -1px;
+   min-height: 32px;
+   display: flex;
+   align-items: center;
+   padding:0 8px;
+ }
+  > .content{
+    padding: 8px;
+  }
+  &:first-child{
+    > .title{
+      border-top-left-radius: 4px;
+      border-top-right-radius: 4px;
+    }
+  }
+  &:last-child{
+    > .title:last-child{
+      border-bottom-left-radius: 4px;
+      border-bottomright-radius: 4px;
+    }
+  }
+
+}
+</style>
