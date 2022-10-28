83ecd057a789534792e7ca3ef19f92c75a1e0fad
yuqian <584882416@qq.com>
2022-10-28 10:15:39 +0800

    添加single选项

--- src/collapse.vue
+++ src/collapse.vue
@@ -4,8 +4,26 @@
   </div>
 </template>
 <script>
+import Vue from 'vue'
 export default {
-
+  props:{
+    single:{
+      type:Boolean,
+      default:false,
+    }
+  },
+data(){
+  return {
+    eventBus :new Vue()
+  }
+},
+provide(){
+  if(this.single){
+    return{
+      eventBus:this.eventBus
+    }
+  }
+}
 }
 </script>
 <style lang="scss" scoped>
