94a09b7a80d35fd66cc1a86ac10cad0c868caafc
yuqian <584882416@qq.com>
2023-01-17 16:54:53 +0800

    完成表单验证组件

--- src/demo.vue
+++ src/demo.vue
@@ -25,63 +25,86 @@
 <!--    </div>-->
 <!--    {{selected.map(item=>item.name)}}-->
 <!--  </div>-->
+<!--<template>-->
+<!--  <div>-->
+<!--    <g-nav :selected.sync="selected">-->
+<!--      <g-nav-item name="home">主页</g-nav-item>-->
+<!--      <g-sub-nav name="about">-->
+<!--        <template slot="title">关于</template>-->
+<!--        <template>-->
+<!--        <g-nav-item name="culture">企业文化</g-nav-item>-->
+<!--        <g-nav-item name="developers">开发团队</g-nav-item>-->
+<!--        </template>-->
+<!--          <g-sub-nav name="contact">-->
+<!--            <template slot="title">联系方式</template>-->
+<!--            <g-nav-item name="wechat">微信</g-nav-item>-->
+<!--            <g-nav-item name="qq">qq</g-nav-item>-->
+<!--            <g-sub-nav name="phone">-->
+<!--              <template slot="title">电话</template>-->
+<!--              <g-nav-item name="cm">移动</g-nav-item>-->
+<!--              <g-nav-item name="cu">联通</g-nav-item>-->
+<!--              <g-nav-item name="cn">电信</g-nav-item>-->
+<!--            </g-sub-nav>-->
+
+
+<!--          </g-sub-nav>-->
+
+
+<!--      </g-sub-nav>-->
+
+<!--      <g-nav-item name="hire">招聘</g-nav-item>-->
+<!--    </g-nav>-->
+
+<!--  <g-nav :selected.sync="selected" vertical style=" margin: 20px; margin-top: 200px;">-->
+<!--    <g-nav-item name="home">首页</g-nav-item>-->
+<!--    <g-sub-nav name="about">-->
+<!--      <template slot="title">关于</template>-->
+<!--      <g-nav-item name="culture">企业文化</g-nav-item>-->
+<!--      <g-nav-item name="developers">开发团队</g-nav-item>-->
+<!--      <g-sub-nav name="contacts">-->
+<!--        <template slot="title">联系方式</template>-->
+<!--        <g-nav-item name="wechat">微信</g-nav-item>-->
+<!--        <g-nav-item name="qq">QQ</g-nav-item>-->
+<!--        <g-sub-nav name="phone">-->
+<!--          <template slot="title">手机</template>-->
+<!--          <g-nav-item name="cm">移动</g-nav-item>-->
+<!--          <g-nav-item name="cu">联通</g-nav-item>-->
+<!--          <g-nav-item name="cn">电信</g-nav-item>-->
+<!--        </g-sub-nav>-->
+<!--      </g-sub-nav>-->
+<!--    </g-sub-nav>-->
+<!--    <g-nav-item name="hire">招聘</g-nav-item>-->
+<!--  </g-nav>-->
+<!--  </div>-->
+<!--</template>-->
 <template>
-  <div>
-    <g-nav :selected.sync="selected">
-      <g-nav-item name="home">主页</g-nav-item>
-      <g-sub-nav name="about">
-        <template slot="title">关于</template>
-        <template>
-        <g-nav-item name="culture">企业文化</g-nav-item>
-        <g-nav-item name="developers">开发团队</g-nav-item>
-        </template>
-          <g-sub-nav name="contact">
-            <template slot="title">联系方式</template>
-            <g-nav-item name="wechat">微信</g-nav-item>
-            <g-nav-item name="qq">qq</g-nav-item>
-            <g-sub-nav name="phone">
-              <template slot="title">电话</template>
-              <g-nav-item name="cm">移动</g-nav-item>
-              <g-nav-item name="cu">联通</g-nav-item>
-              <g-nav-item name="cn">电信</g-nav-item>
-            </g-sub-nav>
-
-
-          </g-sub-nav>
-
-
-      </g-sub-nav>
-
-      <g-nav-item name="hire">招聘</g-nav-item>
-    </g-nav>
-
-  <g-nav :selected.sync="selected" vertical style=" margin: 20px; margin-top: 200px;">
-    <g-nav-item name="home">首页</g-nav-item>
-    <g-sub-nav name="about">
-      <template slot="title">关于</template>
-      <g-nav-item name="culture">企业文化</g-nav-item>
-      <g-nav-item name="developers">开发团队</g-nav-item>
-      <g-sub-nav name="contacts">
-        <template slot="title">联系方式</template>
-        <g-nav-item name="wechat">微信</g-nav-item>
-        <g-nav-item name="qq">QQ</g-nav-item>
-        <g-sub-nav name="phone">
-          <template slot="title">手机</template>
-          <g-nav-item name="cm">移动</g-nav-item>
-          <g-nav-item name="cu">联通</g-nav-item>
-          <g-nav-item name="cn">电信</g-nav-item>
-        </g-sub-nav>
-      </g-sub-nav>
-    </g-sub-nav>
-    <g-nav-item name="hire">招聘</g-nav-item>
-  </g-nav>
+  <div class="form-wrapper">
+    <form class="form" @submit.prevent="onSubmit">
+      <h1>登录</h1>
+      <demo-form-row label="邮箱" :error="errors.email">
+        <g-input type="text" v-model="user.email"></g-input>
+      </demo-form-row>
+      <demo-form-row label="密码" :error="errors.password">
+        <g-input type="password" v-model="user.password"></g-input>
+      </demo-form-row>
+      <div>
+        <g-button class="ok" type="submit">提交</g-button>
+      </div>
+    </form>
   </div>
 </template>
+<style>
+body {background: #888;}
+</style>
 <script>
+import GButton from './button'
+import GInput from './input'
+import DemoFormRow from './demo-form-row'
+import formMixin from './form-mixin'
+import Button from './button'
 import GNav from '../src/nav/nav'
 import GNavItem from '../src/nav/nav-item'
 import GSubNav from '../src/nav/subnav'
-import Button from './button'
 import Cascader from './cascader'
 import Icon from './icon'
 import db from './db'
@@ -109,10 +132,27 @@ import {removeListener} from './click-outside'
 // }
  export default {
   name:'demo',
-   components:{GNav,GNavItem,GSubNav},
+   components: {GButton, GInput, DemoFormRow},
+   mixins: [formMixin],
    data(){
-    return {selected:'home'}
-}
+    return {user: {
+        email: '',
+        password: ''
+      },
+      rules: [
+        {key: 'email', pattern: 'email', required: true},
+        {key: 'password', minLength: 6, required: true}
+      ]
+    };
+  },
+   methods:{
+     onSubmit () {
+       this.validate(this.user)
+       console.log(this.errors)
+     }
+   },
+
+
  }
 </script>
 //   components:{
@@ -168,6 +208,23 @@ html{
 body{
   font-size: 14px;
 }
+.form {
+  background: white;
+  padding: 24px;
+  border-radius: 8px;
+  margin-top: 36px;
+  min-height: 60vh;
+&-wrapper {
+   display: flex;justify-content: center;
+ }
+.controls {
+}
+.ok {
+  display: block;
+  width: 100%;
+  margin-top: 24px;
+}
+}
 .wrapper{
   margin: 40px;
 }
@@ -180,4 +237,5 @@ body{
   align-items: center;
   font-size: 30px;
 }
+
 </style>
